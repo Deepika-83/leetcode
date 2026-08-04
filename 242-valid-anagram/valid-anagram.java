8c6bdcf369d9bb575150147316f0d21c71d608e3
Deepika-83 <gopanaboyinadeepikayadav@gmail.com>
2026-08-04 11:03:52 +0530

    Time: 16 ms (20.72%) | Memory: 46.5 MB (25.78%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,16 +3,19 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int[] freq=new int[26];
-        for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]!=0){
+        for(char ch:t.toCharArray()){
+            if(!map.containsKey(ch)){
                 return false;
             }
+            map.put(ch,map.get(ch)-1);
+            if(map.get(ch)==0){
+                map.remove(ch);
+            }
         }
-        return true;
+        return map.isEmpty();
     }
 }
