947c806fc466d052b758a16f54140e5fe15a64f2
Deepika-83 <gopanaboyinadeepikayadav@gmail.com>
2026-08-08 20:42:26 +0530

    Time: 1 ms (80.92%) | Memory: 47.6 MB (18.62%) - LeetSync

---
+++ 3188-find-champion-i/find-champion-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findChampion(int[][] grid) {
+        int n=grid.length;
+        int c=0,value=0,mx=0;
+        for(int i=0;i<n;i++){
+            c=0;
+            for(int j=0;j<n;j++){
+                if(i!=j && grid[i][j]==1){
+                    c++;
+                }
+            }
+            if(c>mx){
+                mx=c;
+                value=i;
+            }
+        }
+            return value;
+
+    }
+}
