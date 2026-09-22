4794d33749962df49df09b9ce57cba65a9a81535
Deepika-83 <gopanaboyinadeepikayadav@gmail.com>
2026-09-22 18:02:16 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (34.50%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,12 +1,20 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(char i:s.toCharArray()){
-            if(map.containsKey(i)){
-                return i;
+        // HashMap<Character,Integer> map=new HashMap<>();
+        // for(char i:s.toCharArray()){
+        //     if(map.containsKey(i)){
+        //         return i;
+        //     }
+        //     map.put(i,map.getOrDefault(i,0)+1);
+        // }
+        // return 'd';
+        int[] arr=new int[26];
+        for(char ch:s.toCharArray()){
+            if(arr[ch-'a']>=1){
+                return ch; 
             }
-            map.put(i,map.getOrDefault(i,0)+1);
+            arr[ch-'a']++;
         }
-        return 'd';
+        return ' ';
     }
 }
